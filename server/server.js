fa0ce0230b53765164f0d4bca1f27c1a05ce9250
Cynthia <66318121+CynthiaSaab@users.noreply.github.com>
2025-03-17 19:06:50 -0500

    test commit
    i made a useless change just to see if local goes to github remote

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hi, Node.js v22.14.0 backend! Connect via API to frontend');
+  res.send('Hi, Node.js v22.14.0 backend! Connect via API to frontend!!!!!! :)');
 });
 
 app.listen(PORT, () => {
