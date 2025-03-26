561146147e234a8f62449e30513e4466e182c704
AmsalMoiz <amsalmoiz@gmail.com>
2025-03-26 02:27:34 -0500

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:3001/api/auth/login", {
+      const response = await fetch("https://sweet-heaven.azurewebsites.net/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
