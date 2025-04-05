9f22f59d25dae82b2f241a4b469f6240a4b3878d
junialvar <jose.alvarado4818@gmail.com>
2025-04-05 16:17:24 -0500

    removing ssl
    cert file no longer needed

--- server/db.js
+++ server/db.js
@@ -8,15 +8,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_DATABASE,
-  ssl: { 
-    rejectUnauthorized: true,
-  },
   // Connection pool settings
   waitForConnections: true,
   connectionLimit: 10, // Maximum number of connections in the pool
   queueLimit: 0, // Unlimited queue
   // Correct timeout setting
   connectTimeout: 60000, // Connection timeout in milliseconds
+  ssl: { rejectUnauthorized: false }
 });
 
 // Handle pool errors
