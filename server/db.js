6881a550886d71fcb451e0b0291adcff45071d7e
junialvar <jose.alvarado4818@gmail.com>
2025-04-06 17:42:02 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,15 +2,20 @@ require('dotenv').config(); // Load environment variables from .env
 
 const mysql = require('mysql2/promise');
 
+
+console.log("Connecting to DB with:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
 // Create a connection pool instead of a single connection
 const pool = mysql.createPool({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_DATABASE,
-  ssl: { 
-    rejectUnauthorized: true,
-  },
   // Connection pool settings
   waitForConnections: true,
   connectionLimit: 10, // Maximum number of connections in the pool
