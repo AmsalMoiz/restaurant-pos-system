990048d34b1ccc5cd1669d38114fd5f2468a7c4e
AmsalMoiz <amsalmoiz@gmail.com>
2025-04-07 18:58:30 -0500

    changed main page login for users to customer login

--- server/auth.js
+++ server/auth.js
@@ -17,16 +17,16 @@ router.post("/login", async (req, res) => { // POST /api/auth/login
       return res.status(500).json({ error: 'Database connection error' });
     }
     const [rows] = await connection.execute( // Query the database
-      "SELECT user_id, password FROM users WHERE email = ?",
+      "SELECT customer_id, password FROM customers WHERE email = ?",
       [email]
     );
 
-    if (rows.length === 0) { // Check if user with email exists
+    if (rows.length === 0) { // Check if customer with email exists
       return res.status(401).json({ error: "Invalid email or password" });
     }
 
-    const user = rows[0]; // Get user from the result
-    if (password !== user.password) {
+    const customer = rows[0]; // Get customer from the result
+    if (password !== customer.password) {
         return res.status(401).json({ error: "Invalid email or password" });
     }
 
