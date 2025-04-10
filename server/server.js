c6dd7a2263abfab43ff81994e7019ba0b44235b3
AmsalMoiz <amsalmoiz@gmail.com>
2025-04-10 13:22:19 -0500

    test to see if frontend build path is the issue, changes to code relating to directory
    frontend build was not loading before

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ app.use("/api/auth", authRoutes); // Include auth routes
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
