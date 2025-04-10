2cb5a5f16e1df071e632db4b9bcd43ed3de3fa23
AmsalMoiz <amsalmoiz@gmail.com>
2025-04-10 14:49:02 -0500

    menu fetch routing change
    if works, change everywhere

--- client/src/components/MenuTest.js
+++ client/src/components/MenuTest.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './menu.css';
 import Navbar from './Navbar';
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3001' : `http://${window.location.hostname}:3001`;
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3001' : ``;
 
 const MenuTest = ({ cartItems, setCartItems }) => {
   const [desserts, setDesserts] = useState([]);
