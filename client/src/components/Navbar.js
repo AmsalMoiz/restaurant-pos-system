00324e2b37b6fbc85cbfbdb6c4979e26fb235802
Cynthia <66318121+CynthiaSaab@users.noreply.github.com>
2025-04-15 19:58:40 -0500

    Update Navbar.js
    updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,8 +9,8 @@ const Navbar = () => {
       <h1 className="navbar-logo" onClick={() => window.location.href = '/home'} style={{ cursor: 'pointer' }}>Sweet Heaven</h1>
       </div>
       <div className="navbar-right">
-        <Link to="/menu">View Menu</Link>
-        <Link to="/book-table">Book Table</Link> {/* ← PUT IT HERE */}
+        <Link to="/menu">Menu</Link>
+        <Link to="/book-table">Reservations</Link> {/* ← PUT IT HERE */}
         <Link to="/cart">Bag</Link>
       </div>
     </nav>
