a7197274ac3ff1bc259a93fa03f9d72ff577537b
Cynthia <66318121+CynthiaSaab@users.noreply.github.com>
2025-04-16 16:18:58 -0500

    Update App.js
    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
-import ScrollToTop from './ScrollToTop'; 
+import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
 import Login from './components/Login';
 import Menu from './components/MenuTest';
