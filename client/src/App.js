823f21553d9f0bb3560d9c62e4a58d82afd17f75
Cynthia <66318121+CynthiaSaab@users.noreply.github.com>
2025-04-16 16:16:56 -0500

    scroll to top
    added scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
+import ScrollToTop from './ScrollToTop'; 
 import Home from './components/Home';
 import Login from './components/Login';
 import Menu from './components/MenuTest';
