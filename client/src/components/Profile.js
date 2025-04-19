02a523a97f01d218a89fe2a1edb209df41f2efee
AmsalMoiz <amsalmoiz@gmail.com>
2025-04-19 17:58:15 -0500

    no warning

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './profile.css';
 import EditProfileModal from './EditProfileModal';
 import DeleteAccountModal from './DeleteAccountModal';
